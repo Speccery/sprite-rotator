ecaff4fef5066b567bd1f544b1c1ab50a18d957e
Erik Piehl <epiehl@gmail.com>
2022-07-24 15:23:34 +0300

    Initial Commit

---
+++ sprite-rotator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sprite-rotator
+//
+//  Created by Erik Piehl on 24.7.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
